795846e8818a513a4092280bf35adb47a5d2e435
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-03-12 19:43:50 +0900

    Initial commit

---
+++ APG4b/APG4b_ex3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << 3 / 2 * 4 << endl; // 4
+  cout << 3 * 4 / 2 << endl; // 6
+  cout << endl;
+  cout << 1 / 2 * 100 * (100 + 1) << endl; // 0
+  cout << 1.0 / 2 * 100 * (100 + 1) << endl; // 5050
+  cout << 100 * (100 + 1) / 2 << endl; // 5050
+}
---
+++ APG4b/APG4b_ex4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int yearInSec = 365 * 24 * 60 * 60;
+  cout << yearInSec << endl;
+  cout << yearInSec * 2 << endl;
+  cout << yearInSec * 5 << endl;
+  cout << yearInSec * 10 << endl;
+}
