362396a31a1445f25f31438db868ae7c80d615be
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-03-12 22:08:45 +0900

    Remove redundant checking

--- abc/abc088_a.cpp
+++ abc/abc088_a.cpp
@@ -4,13 +4,5 @@ using namespace std;
 int main() {
   int n, a;
   cin >> n >> a;
-
-  if (n >= 500) n = n % 500;
-  if (n <= a) {
-    cout << "Yes";
-  } else {
-    cout << "No";
-  }
-
-  cout << endl;
+  cout << (n % 500 <= a ? "Yes" : "No") << endl;
 }
