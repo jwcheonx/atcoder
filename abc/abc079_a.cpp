395dae1a213bfbcae3f1156795e7f5a5d95ed299
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-03-15 16:44:02 +0900

    Add abc079_a.cpp

---
+++ abc/abc079_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string n; cin >> n;
+
+  if (n[0] == n[1] && n[1] == n[2]) {
+    cout << "Yes";
+  } else if (n[1] == n[2] && n[2] == n[3]) {
+    cout << "Yes";
+  } else {
+    cout << "No";
+  }
+
+  if (n[1] == n[2] &&
+     (n[0] == n[1] || n[2] == n[3])) {
+    cout << "Yes";
+  } else {
+    cout << "No";
+  }
+
+  cout << endl;
+}
