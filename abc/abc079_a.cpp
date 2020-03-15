6c009f06308a9d94afc93c1ba39fba302753c1a3
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-03-15 16:48:28 +0900

    Comment out unused code

--- abc/abc079_a.cpp
+++ abc/abc079_a.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
   string n; cin >> n;
 
-  if (n[0] == n[1] && n[1] == n[2]) {
-    cout << "Yes";
-  } else if (n[1] == n[2] && n[2] == n[3]) {
-    cout << "Yes";
-  } else {
-    cout << "No";
-  }
+  // if (n[0] == n[1] && n[1] == n[2]) {
+  //   cout << "Yes";
+  // } else if (n[1] == n[2] && n[2] == n[3]) {
+  //   cout << "Yes";
+  // } else {
+  //   cout << "No";
+  // }
 
   if (n[1] == n[2] &&
      (n[0] == n[1] || n[2] == n[3])) {
