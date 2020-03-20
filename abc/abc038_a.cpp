d8f3978f63affcc7d5ce44ffbfe0577c1089ef16
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-03-20 20:47:01 +0900

    Add abc038_a.cpp

---
+++ abc/abc038_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (s.at(s.size() - 1) == 'T') {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+
+  cout << endl;
+}
