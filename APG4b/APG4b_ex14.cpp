419a5794a6bf6194ef9da9be1fd4ed19a8f3b9c0
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-04-15 21:33:22 +0900

    Add APG4b_ex14.cpp

---
+++ APG4b/APG4b_ex14.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  vector<int> v(3);
+  rep(i, 3) cin >> v.at(i);
+  sort(v.begin(), v.end());
+  cout << v.back() - v.front() << endl;
+}
