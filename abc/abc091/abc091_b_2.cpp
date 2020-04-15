d5770c1cf6c8844c2109b9ad10b2439cfe3ec7d3
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-04-15 18:00:00 +0900

    Add abc091_b_2.cpp

---
+++ abc/abc091/abc091_b_2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n, m;
+  map<string, int> mp;
+
+  cin >> n;
+  rep(i, n) {
+    string s; cin >> s;
+    mp[s]++;
+  }
+
+  cin >> m;
+  rep(i, m) {
+    string t; cin >> t;
+    mp[t]--;
+  }
+
+  int ans = 0;
+  for (auto it : mp){
+    ans = max(ans, it.second);
+  }
+
+  cout << ans << endl;
+}
