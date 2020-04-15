f717f7f6743e87c61aee1e2c1a8a7ca4e09210bb
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-04-15 17:30:44 +0900

    Add abc091_b.cpp

---
+++ abc/abc091_b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n; cin >> n;
+  vector<string> s(n);
+  rep(i, n) cin >> s.at(i);
+  sort(s.begin(), s.end());
+
+  int m; cin >> m;
+  vector<string> t(m);
+  rep(i, m) cin >> t.at(i);
+
+  int x = 0;
+  string checked = "";
+  rep(i, n) {
+    string cur = s.at(i);
+    if (cur == checked) continue;
+
+    int earn = count(s.begin(), s.end(), cur);
+    int lose = count(t.begin(), t.end(), cur);
+    if (x < earn - lose) x = earn - lose;
+
+    checked = cur;
+  }
+
+  cout << x << endl;
+}
