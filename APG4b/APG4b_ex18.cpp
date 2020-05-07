8503c5c944e45526bae4222870fb2ce594474cba
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-05-07 14:40:40 +0900

    Add APG4b_ex18.cpp

---
+++ APG4b/APG4b_ex18.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<char>> ans(n, vector<char>(n, '-'));
+
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--; b--;
+    ans.at(a).at(b) = 'o';
+    ans.at(b).at(a) = 'x';
+  }
+
+  rep(i, n) rep(j, n) {
+    cout << ans.at(i).at(j);
+    if (j == n - 1) cout << endl;
+    else cout << " ";
+  }
+}
