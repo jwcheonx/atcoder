1face5c6b1e037bf2d3077e588c4f83acdceaf7b
Junwoo Cheon <neko2ita.dev@gmail.com>
2020-05-24 16:53:18 +0900

    Add alternative implementation

--- APG4b/recursion/r02.cpp
+++ APG4b/recursion/r02.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int sum_range(int a, int b) {
   if (a == b) return a;
-  return b + sum_range(a, b - 1);
+  return b + sum_range(a, b - 1); // (sum of a to b) = b + (sum of a to (b - 1))
 }
+
+// int sum_range(int a, int b) {
+//   if (a == b) return a;
+//   return a + sum_range(a + 1, b); // (sum of a to b) = a + (sum of (a + 1) to b)
+// }
 
 int main() {
   cout << sum_range(3, 7) << endl; // 3 + 4 + 5 + 6 + 7 = 25
+  cout << sum_range(5, 8) << endl; // 5 + 6 + 7 + 8 = 26
 }
